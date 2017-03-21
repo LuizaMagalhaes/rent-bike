cf21f02e31bd2e3646e0347bc966a91827700654
Luiza e Jeny <luizalmagalhaes@hotmail.com>
2017-03-20 22:03:04 -0300

    Create factories

---
+++ spec/factories/bikes.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory :bike do
+    color 'preta'
+    weel_size '26'
+    condition 'Nova'
+    category 'Adulto'
+    user_for 'Passeio'
+  end
+
+end
---
+++ spec/features/user_search_bicicles_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature 'User Search For Bicicles' do
+
+  scenario 'successfully' do
+    #setup
+    bike = create(:bike)
+
+    #execution
+
+
+    #expectation
+
+
+  end
+
+end

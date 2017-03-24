9d24fc593e4714dbf6a679cefa15154b36b2b60d
Luiza <luizalmagalhaes@hotmail.com>
2017-03-23 21:10:09 -0300

    bike already rented

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -7,12 +7,25 @@ class RentsController < ApplicationController
 
   def create
     @advertisement = Advertisement.find(params[:advertisement_id])
-    rent = @advertisement.rents.create(rent_params)
-    rent.price = @advertisement.price
-    rent.save
-    flash[:success] = "Aluguel Confirmado por R$ #{rent.price},
-    na data #{rent.rent_date}"
-    redirect_to [@advertisement, rent]
+    rent_date = params[:rent][:rent_date]
+    rented = false
+    @advertisement.rents.each do |rent|
+      if rent.rent_date = rent_date
+        rented = true
+      end
+    end
+
+    unless rented
+      rent = @advertisement.rents.create(rent_params)
+      rent.price = @advertisement.price
+      rent.save
+      flash[:success] = "Aluguel Confirmado por R$ #{rent.price},
+      na data #{rent.rent_date}"
+      redirect_to [@advertisement, rent]
+    else
+      flash[:error] = "Ops... Essa bike já está alugada no dia #{rent_date} :("
+      redirect_to @advertisement
+    end
   end
 
   def show
--- spec/features/user_rent_a_bike_spec.rb
+++ spec/features/user_rent_a_bike_spec.rb
@@ -5,9 +5,9 @@ feature 'User rents a bike' do
     bike = create(:bike)
     advertisement = create(:advertisement, bike: bike)
     customer = build(:customer)
-    rent = build(:rent, customer: customer, payment: 'Dinheiro', rent_date:'29/04/17')
 
-    visit advertisement_path(advertisement)
+    visit root_path
+    click_on advertisement.title
 
     click_on 'Alugar Bike'
 
@@ -15,18 +15,43 @@ feature 'User rents a bike' do
     fill_in 'Telefone',         with: customer.phone
     fill_in 'CPF',              with: customer.cpf
     fill_in 'E-mail',           with: customer.email
-    fill_in 'Pagamento',        with: rent.payment
-    fill_in 'Data do Aluguel',  with: rent.rent_date
+    fill_in 'Pagamento',        with: 'Dinheiro'
+    fill_in 'Data do Aluguel',  with: '01/01/2017'
 
 
     click_on 'Confirmar Aluguel'
 
     expect(page).to have_content('Aluguel Confirmado por')
-    expect(page).to have_content rent.payment
-    expect(page).to have_content rent.rent_date
+    expect(page).to have_content 'Dinheiro'
+    expect(page).to have_content '01/01/2017'
     expect(page).to have_content customer.name
     expect(page).to have_content customer.phone
     expect(page).to have_content customer.cpf
     expect(page).to have_content customer.email
+  end
+
+  scenario 'bike already rented' do
+
+    bike = create(:bike)
+    customer = create(:customer)
+    another_customer = create(:customer, name: 'Joao', phone: '999888777', cpf: '987654311')
+    ad = create(:advertisement, bike: bike)
+    rent = create(:rent, advertisement: ad, customer: customer, rent_date: '20/04/2017')
+
+    visit root_path
+    click_on ad.title
+    click_on 'Alugar Bike'
+
+    fill_in 'Nome',             with: customer.name
+    fill_in 'Telefone',         with: customer.phone
+    fill_in 'CPF',              with: customer.cpf
+    fill_in 'E-mail',           with: customer.email
+    fill_in 'Pagamento',        with: 'Dinheiro'
+    fill_in 'Data do Aluguel',  with: '20/04/2017'
+
+
+    click_on 'Confirmar Aluguel'
+    
+    expect(page).to have_content 'Ops... Essa bike já está alugada no dia 20/04/2017 :('
   end
 end

5d45a8687a45f54dc314c7ceffa759f769e7eb1c
Luiza Lima Magalhaes <Luiza@MacBook-Pro-de-Luane.local>
2017-03-27 17:01:03 -0300

    fix the layout problems

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,5 +1,6 @@
 class AdvertisementsController < ApplicationController
   def show
+    @advertisements = Advertisement.all
     @advertisement = Advertisement.find(params[:id])
     @bike = Bike.find(params[:id])
     @availabilities = 7.times.map do |n|
